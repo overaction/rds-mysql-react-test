75a698d5002e23b2decbf33e2b44b00a535f14ac
mckim <kmc04@naver.com>
2021-05-20 01:58:58 +0900

    input form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [state, setstate] = useState({
+    name: '',
+    number: ''
+  })
+  const {name, number} = state;
+  const onChange = (e) => {
+    const {value,name} = e.target;
+    setstate({
+      ...state,
+      [name]: value
+    })
+    console.log(state)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form method='post' action='http://localhost:3001'>
+            <div className='name'>
+              <label htmlFor='name'>Enter Name:</label>
+              <input type='text' name='name' onChange={onChange}/>
+            </div>
+            <div className='number'>
+              <label htmlFor='rollno'>Enter Number:</label>
+              <input type='text' name='number' onChange={onChange}/>
+            </div>
+            <div className='submit'>
+              <input type='submit'/>
+            </div>            
+        </form>   
     </div>
   );
 }
